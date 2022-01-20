e17fe428bb679cd753d5d02e090ff5a4ebb0be94
johnnovosel <johnnovosel96@gmail.com>
2022-01-19 23:29:49 -0600

    Added parsing of text file and adding to map to find the total count of letters

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+
+var text = fs.readFileSync("./words.txt", 'utf-8');
+var textByLine = text.split('\n');
+const wordMap = new Map();
+
+var totalLetters = 0;
+
+
+textByLine.forEach(string => {
+  if(string.length == 5) {
+    var stringArr = string.split("");
+    for (var i = 0; i < stringArr.length; i++) {
+      ++totalLetters;
+      if(wordMap.get(stringArr[i]) == null) {
+        wordMap.set(stringArr[i], 1);
+      } else {
+        wordMap.set(stringArr[i], wordMap.get(stringArr[i]) + 1);
+      }
+  
+    }
+  }
+})
+
+
+wordMap.forEach((values,keys) => {
+  var percentage = (100 * values) / totalLetters;
+
+  console.log(keys + " | " + Math.round(percentage * 100) / 100 + "%");
+
+
+})
