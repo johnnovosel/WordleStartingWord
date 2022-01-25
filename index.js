d87cbcd988da48bbcb5bcbb7ea053ec614724616
johnnovosel <johnnovosel96@gmail.com>
2022-01-25 12:21:04 -0600

    cleaned up the code

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 var fs = require('fs');
 
+var pathPast = "./PastWordles.txt";
+var pathAll = "./ShuffledWordles.txt";
 
-var text = fs.readFileSync("./words.txt", 'utf-8');
-var textByLine = text.split('\n');
-const wordMap = new Map();
+console.log("Past Wordle Letter Distribution: \n")
+printLetters(pathPast);
+console.log("___________________________________________________________\n");
+console.log("All Wordle Letter Distribution: \n")
+printLetters(pathAll);
 
-var totalLetters = 0;
+function printLetters(filePath) {
+  var wordles = fs.readFileSync(filePath, 'utf-8');
+  var wordlesArr = wordles.split(/\n|\,/);
 
+  const wordlesMap = new Map();
 
-textByLine.forEach(string => {
-  if(string.length == 5) {
+  var totalLetters = 0;
+
+  wordlesArr.forEach(string => {
+    if(string.length != 5) return;
+
     var stringArr = string.split("");
     for (var i = 0; i < stringArr.length; i++) {
       ++totalLetters;
-      if(wordMap.get(stringArr[i]) == null) {
-        wordMap.set(stringArr[i], 1);
+      if(wordlesMap.get(stringArr[i]) == null) {
+        wordlesMap.set(stringArr[i], 1);
       } else {
-        wordMap.set(stringArr[i], wordMap.get(stringArr[i]) + 1);
+        wordlesMap.set(stringArr[i], wordlesMap.get(stringArr[i]) + 1);
       }
-  
     }
-  }
-})
-
-const sortedMap = new Map([...wordMap.entries()].sort((a, b) => b[1] - a[1]));
-
-sortedMap.forEach((values,keys) => {
-  var percentage = (100 * values) / totalLetters;
-
-  console.log(keys + " | " + Math.round(percentage * 100) / 100 + "%");
+  })
 
 
-})
+  const sortedMapPast = new Map([...wordlesMap.entries()].sort((a, b) => b[1] - a[1]));
+
+  sortedMapPast.forEach((values,keys) => {
+    var percentage = (100 * values) / totalLetters;
+
+    console.log(keys + " | " + Math.round(percentage * 100) / 100 + "%");
+  })
+}
